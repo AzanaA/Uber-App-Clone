c34abf50ed56ac9dc02a2d90e7c9f8af16d163c3
Azana Alleyne <zizie19@gmail.com>
2023-06-22 00:24:27 -0400

    added images, GoogleDestinationSearch API and Map

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,39 +1,52 @@
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, Image} from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, FlatList, Image } from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+import { ArrowRightOutlined } from '@ant-design/icons';
+import { Icon } from '@rneui/base';
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
     {
     id: "123",
     title: "Get a ride",
-    image: null,
+    image: "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_698,h_465/v1568070387/assets/b5/0a5191-836e-42bf-ad5d-6cb3100ec425/original/UberX.png",
     screen: "MapScreen"
     },
     {
         id: "456",
         title: "Order food",
-        image: null,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5jP2Ko1eg-rcdK8Um3WX9aW7CGANWsHjS5wN4kwy6hSTQI8XWRex0kiRBvZrqxd6x-Gc&usqp=CAU",
         screen: "EatsScreen"
     }
 ];
  
 const NavOptions = () => {
+  const navigation = useNavigation();
   return (
-    <FlatList 
-        data={data}
-        horizontal
-        keyExtractor={(item) =>item.id} 
-        renderItem={({ item }) => (
-            <TouchableOpacity>
-              <View>
-                <Image 
-                style={{width: 120, height: 120, resizeMode: "contain"}}
-                  source={{uri: item.image}}
-                />
-
-              </View>
-            </TouchableOpacity>
-        )}
-    />
+    <SafeAreaView>
+      <FlatList 
+          data={data}
+          horizontal
+          keyExtractor={(item) => item.id} 
+          renderItem={({ item }) => (
+              <TouchableOpacity 
+                style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+                onPress={() => navigation.navigate(item.screen)}>
+                <View>
+                  <Image 
+                  style={{width: 120, height: 120, resizeMode: "contain"}}
+                    source={{uri: item.image}}
+                  />
+                  <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                 {/*  <ArrowRightOutlined/> */} {/* Need to make this work */}
+                 <Icon 
+                 style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                 name="arrowright" color="white" type="antdesign" />
+                </View>
+              </TouchableOpacity>
+          )}
+      />
+    </SafeAreaView>
   )
 };
 
